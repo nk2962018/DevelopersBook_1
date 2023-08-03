ef8a8625e83ddaa33e86ee5d3eaaa38a43551c3c
Nisha <nkolivia1234@gmail.com>
2023-08-03 20:57:49 +0530

    Refactor:group similar tasks in testcase file.

--- src/testcases/DevelopersBook.spec.js
+++ src/testcases/DevelopersBook.spec.js
@@ -2,16 +2,17 @@ import { render, screen } from '@testing-library/react';
 import DevelopersBook from '../componenets/DevelopersBook';
 
 describe('Developer Book works fine when', () => {
+  beforeEach(() => {
+    render(<DevelopersBook/>)
+  });
 
   it('it renders title with proper text' , () => {
-  render(<DevelopersBook/>);
   const title = screen.getByTestId('title')
   expect(title.textContent).toBe('Developers Book')
 
   });
 
   it('it displays all the 5 books' , () => {
-    render(<DevelopersBook/>);
     const books = screen.getAllByTestId('book')
     expect(books).toHaveLength(5)
   });
